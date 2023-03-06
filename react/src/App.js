0af90596fd3639bcedfa931b3da6ebb3acd0496b
Anelka <aaarnold@student.fullsail.edu>
2023-03-05 22:02:47 -0500

    Some styling & Spotify developer account setup

--- react/src/App.js
+++ react/src/App.js
@@ -1,16 +1,27 @@
-
-import './App.css';
+import Format from "./layout/format";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      <p>Coming Soon!</p>
-       <p>
-        Spotify Web-API - Anelka Arnold
-       </p>
-      
+      <Format>
+      <header className="Header">
+        <img src="./spotifylogo.jpeg" alt="logo" className="Logo">
+        </img>
+      <input type="text" placeholder="Search..." className="Search"/>
       </header>
+      <div className="Container">
+        <p>Coming Soon!</p>
+         <p>
+          Spotify Web-API - Anelka Arnold
+         </p>
+      </div>
+      <footer className="Footer">
+        <img src="./spotifylogo.jpeg" alt="logo" className="Logo">
+        </img>
+      <input type="text" placeholder="Search..." className="Search"/>
+      </footer>
+
+      </Format>
     </div>
   );
 }
