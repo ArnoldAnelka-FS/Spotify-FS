5b2e1c2d0e3b88aa6fd96915a98a697b8641cc4f
Anelka <aaarnold@student.fullsail.edu>
2023-03-10 01:42:37 -0500

    open auth finished
    closes #5

--- react/src/App.js
+++ react/src/App.js
@@ -1,22 +1,38 @@
+import { useEffect, useState } from "react";
 import Format from "./layout/format";
 
+
 function App() {
+  const Client_ID = `f6e66b16d04645a18ef3f591e8f78abc`
+  const REDIRECT_URI = "http://localhost:3000"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState(useState,"")
+
+
+
+
+
+
   return (
     <div className="App">
       <Format>
       <header className="Header">
-        <img src="./spotifylogo.jpeg" alt="logo" className="Logo">
+        <img src="./spotify.svg" alt="logo" className="Logo">
         </img>
       <input type="text" placeholder="Search..." className="Search"/>
       </header>
       <div className="Container">
-        <p>Coming Soon!</p>
-         <p>
+
+      <p>
           Spotify Web-API - Anelka Arnold
          </p>
+        <a href={`${AUTH_ENDPOINT}?client_id=${Client_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}> Login to Spotify</a>
+        
       </div>
       <footer className="Footer">
-        <img src="./spotifylogo.jpeg" alt="logo" className="Logo">
+        <img src="./spotify.svg" alt="logo" className="Logo">
         </img>
       <input type="text" placeholder="Search..." className="Search"/>
       </footer>
