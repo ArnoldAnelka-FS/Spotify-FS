f04bccdfb35595a5049a00c586c6fe637d5ca1f3
Anelka <aaarnold@student.fullsail.edu>
2023-03-20 23:39:27 -0400

    spotify router endpoints added

--- server/routes/spotifyRouter.js
+++ server/routes/spotifyRouter.js
@@ -3,8 +3,8 @@ const spotifyRouter = require('./routes/spotify');
 const spotifyController = require('./controllers/spotifyController');
 
 spotifyRouter.get("/login", spotifyController.login);
-// spotifyRouter.get("/auth", spotifyController.auth);
-// spotifyRouter.get("/token", spotifyController.token);
-// spotifyRouter.get("/search", spotifyController.search);
+spotifyRouter.get("/auth", spotifyController.auth);
+spotifyRouter.get("/token", spotifyController.token);
+spotifyRouter.get("/search", spotifyController.search);
 
 module.exports = spotifyRouter;
